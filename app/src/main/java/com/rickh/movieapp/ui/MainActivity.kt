f2720bdd234375662fedef9c5cd9267c5a85f4bf
Rick Huisman <rick.huisman@live.nl>
2019-09-17 19:42:48 +0200

    Add PopupMenu to toolbar for sort options (Non-functional)

--- app/src/main/java/com/rickh/movieapp/ui/MainActivity.kt
+++ app/src/main/java/com/rickh/movieapp/ui/MainActivity.kt
@@ -2,13 +2,22 @@ package com.rickh.movieapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.ArrayAdapter
-import com.rickh.movieapp.R
+import androidx.appcompat.widget.PopupMenu
 import com.rickh.movieapp.ui.movies.MoviesFragment
 import com.rickh.movieapp.util.ViewUtils
 import kotlinx.android.synthetic.main.activity_main.*
+import timber.log.Timber
+import android.view.MenuItem
+import android.view.View
+import androidx.core.view.get
+import com.rickh.movieapp.R
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var sortOptionsMenu: PopupMenu
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +25,7 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(toolbar)
         setupSpinner()
+        setupSortMenu()
 
         // Start fragment
         supportFragmentManager.beginTransaction()
@@ -35,6 +45,45 @@ class MainActivity : AppCompatActivity() {
         with(spinner) {
             adapter = categoriesAdapter
             dropDownVerticalOffset = ViewUtils.dpToPix(context, 8)
+        }
+    }
+
+    private fun setupSortMenu() {
+        createSortMenu(sort)
+
+        sort.setOnClickListener {
+            sortOptionsMenu.show()
+        }
+    }
+
+    private fun createSortMenu(v: View) {
+        sortOptionsMenu = PopupMenu(
+            this, v, Gravity.END, 0, R.style.MovieAppPopupMenu_MoviesSort
+        )
+
+        sortOptionsMenu.menuInflater.inflate(R.menu.menu_movies_sorting_mode, sortOptionsMenu.menu)
+        sortOptionsMenu.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.action_movies_sorting_popular -> Timber.d("Popular")
+                R.id.action_movies_sorting_top_rated -> Timber.d("Top rated")
+                R.id.action_movies_sorting_upcoming -> Timber.d("Upcoming")
+                R.id.action_movies_sorting_now_playing -> Timber.d("Now playing")
+            }
+            highlightActiveSorting(menuItem)
+            true
+        }
+    }
+
+    private fun highlightActiveSorting(selMenuItem: MenuItem) {
+        val menu = sortOptionsMenu.menu
+
+        // Reset highlighted items
+        menu.setGroupEnabled(0, true)
+
+        for (i in 0 until menu.size()) {
+            if (selMenuItem.title == menu[i].title) {
+                menu[i].isEnabled = false
+            }
         }
     }
 }
