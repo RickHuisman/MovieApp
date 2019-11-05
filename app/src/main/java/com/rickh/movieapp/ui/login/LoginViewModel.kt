a656c5481488f1b72f22f5643113666f4577a11b
Rick Huisman <rick.huisman@live.nl>
2019-11-05 16:58:41 +0100

    Get user session from tmdb api

---
+++ app/src/main/java/com/rickh/movieapp/ui/login/LoginViewModel.kt
@@ -0,0 +1,19 @@
+package com.rickh.movieapp.ui.login
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import com.omertron.themoviedbapi.model.authentication.TokenSession
+import com.rickh.movieapp.tmdb.AuthenticationRepository
+import com.rickh.movieapp.tmdb.Result
+import kotlinx.coroutines.Dispatchers
+
+class LoginViewModel : ViewModel() {
+
+    private val authRepository = AuthenticationRepository
+
+    fun login(username: String, password: String): LiveData<Result<TokenSession>> =
+        liveData(Dispatchers.IO) {
+            emit(authRepository.login(username, password))
+        }
+}
